670bba1fed39770629963cef33869f2521968f6a
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-09-17 11:27:09 -0600

    Test updates
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Page() {
     return <main style={{ marginLeft: '30rem', marginTop: '10rem' }}>
         <h1 style={{ color: '#68f6c7' }}>
-            Web Dev 2 <i className="font-serif">Assignments</i>
+            Web Dev 2 <span style={{fontStyle: 'italic'}}>Assignments</span>
         </h1>
 
         <ul className="m-6">
