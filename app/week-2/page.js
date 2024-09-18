4a1f0125aeb8b152baca51f8c479b022bc58b4fc
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-09-18 14:39:13 -0600

    More updates
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,10 @@
+import Link from "next/link"
+import StudentInfo from "./student-info"
+
 export default function Page() {
-    return (
-        <main className="m-6">
-            <h1>Shopping List</h1>
-        </main>
-    );
+    return <main className="m-6">
+        <p><Link href="../" >Go back...</Link> </p>
+        <h1>Shopping List</h1>
+        <StudentInfo />
+    </main>
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return <main>
+        <h2>Kris Schneider&apos;s page</h2>
+        <p><Link href="https://github.com/FlawlessCasual17/cprg306-assignments" target="_blank">GitHub repository link</Link></p>
+    </main>
+}
