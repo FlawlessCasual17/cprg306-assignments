28ad0ea7a819367f0359dd64b8a66cbb0b2a0d48
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-09-25 21:23:43 -0600

    Updated `week-3` page
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -3,16 +3,27 @@ import Link from "next/link"
 export default function Page() {
     return (
         <main className="m-6">
-            <Link href="../">Go back...</Link>
-            <div className="text-center" style={{ marginRight: '40rem', marginTop: '10rem' }}>
-                <h1 className="font-serif italic">Shopping List</h1>
-                <ul className="mr-80 text-lg">
-                    <li className="flexbox">Bananas 🍌</li>
-                    <li className="flexbox">Bread 🍞</li>
-                </ul>
+            <Link href="../">Go back... (Shop for more items)</Link>
+            <div style={{ marginRight: '10vw', marginTop: '10rem' }}>
+                <h1 className="text-center font-serif italic left-header">
+                    Shopping List
+                </h1>
+                <div className="text-lg flex-container">
+                    <div className="item">8 Bananas 🍌</div>
+                    <div className="item">1 Bread Loaf 🍞</div>
+                    <div className="item">2 T-Shirts 👕</div>
+                    <div className="item">11 Hammers 🔨</div>
+                    <div className="item">1 Political Poster ⚒️</div>
+                    <div className="item">Gaming Chair 💺</div>
+                </div>
             </div>
             <div className="vertical-line" />
             {/* TODO: Add checkout section on the other side. */}
+            <div style={{ marginLeft: '10vw', marginTop: '10rem' }}>
+                <h1 className="text-center font-serif italic right-header">
+                    Checkout
+                </h1>
+            </div>
         </main>
     )
 }
