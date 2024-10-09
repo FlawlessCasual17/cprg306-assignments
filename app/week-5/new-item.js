a47220f3c5bab4f2f063f4c8bac3eb5659daf518
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-10-09 14:49:39 -0600

    Added `week-5` page
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

---
+++ app/week-5/new-item.js
@@ -0,0 +1,50 @@
+import './styles.css'
+import { useState, useEffect } from 'react'
+
+export default function NewItem() {
+    const [count, setCount] = useState(1)
+    const [isMin, setIsMin] = useState(true)
+    const [isMax, setIsMax] = useState(false)
+
+    const MIN_COUNT = 1, MAX_COUNT = 20
+
+    useEffect(() => {
+        setIsMin(count === MIN_COUNT)
+        setIsMax(count === MAX_COUNT)
+        },
+        [count]
+    )
+
+    const increment = () => setCount(count < MAX_COUNT ? count + 1 : MAX_COUNT)
+    const decrement = () => setCount(count > MIN_COUNT ? count - 1 : MIN_COUNT)
+
+    const buttonClasses = {
+        red: 'border-4 bg-red-800 border-red-900 hover:bg-red-700 hover:border-red-800',
+        blue: 'border-4 bg-blue-600 border-blue-800 hover:bg-blue-500 hover:border-blue-700',
+        gray: 'border-4 bg-gray-600 border-gray-700 hover:bg-gray-500 hover:border-gray-600'
+    };
+
+    const getButtonClass = (baseColor, isLimit) => isLimit ? buttonClasses.red : buttonClasses[baseColor];
+
+    return <div className='m-6'>
+        <div className='box text-center'>
+            <div className='huge p-2.5 rounded'>
+                {count}
+            </div>
+            <div className='buttons'>
+                <button
+                    className={getButtonClass('gray', isMin)}
+                    onClick={decrement}
+                >
+                    -
+                </button>
+                <button
+                    className={getButtonClass('blue', isMax)}
+                    onClick={increment}
+                >
+                    +
+                </button>
+            </div>
+        </div>
+    </div>
+}
---
+++ app/week-5/page.js
@@ -0,0 +1,8 @@
+"use client"
+import './styles.css'
+
+export default function Page() {
+    return <main>
+        <Link className='absolute' href='../'>Go back...</Link>
+    </main>
+}
