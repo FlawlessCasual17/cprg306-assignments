2ca928c90e2fb9b555ffe4e70b1cac8cc07ba611
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-10-08 18:17:48 -0600

    Update code for `week-4` module again
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -7,30 +7,23 @@ export default function Page() {
     let [count, setCount] = useState(1)
 
     // count is the current value of the state
-    // setCount is a function tha updates the state
+    // setCount is a function that updates the state
 
     const increment = () => setCount(count + 1)
-    const decrement = () => setCount(count - 1);
-
-    /*
-     *  The nested ternary operation is equivalent to
-     *  if (count < 20) count
-     *  else if (count > 1) 1
-     *  else 20
-     */
+    const decrement = () => setCount(count - 1)
 
     return <main className='m-6'>
         <Link href='../'>Go back...</Link>
-        <div className='absolute w-1/6 rounded p-5 box text-center justify-center'>
-            <h2 className='p-2.5'>
+        <div className='box size-1/2 p-5 text-center'>
+            <div className='huge p-2.5'>
                 {/* If "count" is greater than 20 or more, the value will be 20. */}
                 {/* If the value of "count" is inclusively between 1 and 19, the value will be itself. */}
                 {/* If "count" is less than 1, the value will be 1. */}
                 {count = count < 20 ? (count > 1 ? count : 1) : 20}
-            </h2>
-            <div className='flex justify-between'>
-                <button className='w-1/2 border rounded bg-gray-600' onClick={decrement}>-</button>
-                <button className='w-1/2 border rounded bg-blue-600 border-blue-800' onClick={increment}>+</button>
+            </div>
+            <div className='buttons'>
+                <button className='border-4 bg-gray-600 border-gray-700' onClick={decrement}>-</button>
+                <button className='border-4 bg-blue-600 border-blue-800' onClick={increment}>+</button>
             </div>
         </div>
     </main>
