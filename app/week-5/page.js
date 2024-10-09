e2c5581c36c90a24bb9e504abe080f9a3a014379
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-10-09 16:40:17 -0600

    Update `week-5` page
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- app/week-5/page.js
+++ app/week-5/page.js
@@ -1,9 +1,11 @@
 "use client"
 import './styles.css'
 import Link from 'next/link'
+import NewItem from './new-item'
 
 export default function Page() {
     return <main>
         <Link className='absolute' href='../'>Go back...</Link>
+        <NewItem/>
     </main>
 }
