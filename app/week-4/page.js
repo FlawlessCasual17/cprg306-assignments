80895d94685f81b7b1e1c6555bd96faa7c070544
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-10-08 21:10:06 -0600

    Update code for `week-4` again
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 import Link from 'next/link'
 import { useState } from 'react'
 import './styles.css'
@@ -13,17 +13,29 @@ export default function Page() {
     const decrement = () => setCount(count - 1)
 
     return <main className='m-6'>
-        <Link href='../'>Go back...</Link>
-        <div className='box size-1/2 p-5 text-center'>
-            <div className='huge p-2.5'>
-                {/* If "count" is greater than 20 or more, the value will be 20. */}
-                {/* If the value of "count" is inclusively between 1 and 19, the value will be itself. */}
-                {/* If "count" is less than 1, the value will be 1. */}
+        <Link className='absolute' href='../'>Go back...</Link>
+        <div className='box text-center'>
+            <div className='huge p-2.5 rounded'>
+                {/* If 'count' is greater than 20 or more, the value will be 20. */}
+                {/* If the value of 'count' is inclusively between 1 and 19, the value will be itself. */}
+                {/* If 'count' is less than 1, the value will be 1. */}
                 {count = count < 20 ? (count > 1 ? count : 1) : 20}
             </div>
             <div className='buttons'>
-                <button className='border-4 bg-gray-600 border-gray-700' onClick={decrement}>-</button>
-                <button className='border-4 bg-blue-600 border-blue-800' onClick={increment}>+</button>
+                <button
+                    className={ 'border-4 bg-gray-600 border-gray-700 ' +
+                        'hover:bg-gray-500 hover:border-gray-600' }
+                    onClick={decrement}
+                >
+                    -
+                </button>
+                <button
+                    className={ 'border-4 bg-blue-600 border-blue-800 ' +
+                        'hover:bg-blue-500 hover:border-blue-700' }
+                    onClick={increment}
+                >
+                    +
+                </button>
             </div>
         </div>
     </main>
