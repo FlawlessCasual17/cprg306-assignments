564826b6edf3b5d2bf3af6e3a462ab49c84acb20
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-10-08 22:03:54 -0600

    Update code for `week-4` again
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -1,42 +1,55 @@
-'use client'
+"use client"
 import Link from 'next/link'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './styles.css'
 
 export default function Page() {
-    let [count, setCount] = useState(1)
+    const [count, setCount] = useState(1)
+    const [isMin, setIsMin] = useState(true)
+    const [isMax, setIsMax] = useState(false)
 
-    // count is the current value of the state
-    // setCount is a function that updates the state
+    const MIN_COUNT = 1, MAX_COUNT = 20
 
-    const increment = () => setCount(count + 1)
-    const decrement = () => setCount(count - 1)
+    useEffect(() => {
+        setIsMin(count === MIN_COUNT)
+        setIsMax(count === MAX_COUNT)
+        },
+        [count]
+    )
 
-    return <main className='m-6'>
-        <Link className='absolute' href='../'>Go back...</Link>
-        <div className='box text-center'>
-            <div className='huge p-2.5 rounded'>
-                {/* If 'count' is greater than 20 or more, the value will be 20. */}
-                {/* If the value of 'count' is inclusively between 1 and 19, the value will be itself. */}
-                {/* If 'count' is less than 1, the value will be 1. */}
-                {count = count < 20 ? (count > 1 ? count : 1) : 20}
+    const increment = () => setCount(count < MAX_COUNT ? count + 1 : MAX_COUNT)
+    const decrement = () => setCount(count > MIN_COUNT ? count - 1 : MIN_COUNT)
+
+    const buttonClasses = {
+        red: 'border-4 bg-red-800 border-red-900 hover:bg-red-700 hover:border-red-800',
+        blue: 'border-4 bg-blue-600 border-blue-800 hover:bg-blue-500 hover:border-blue-700',
+        gray: 'border-4 bg-gray-600 border-gray-700 hover:bg-gray-500 hover:border-gray-600'
+    };
+
+    const getButtonClass = (baseColor, isLimit) => isLimit ? buttonClasses.red : buttonClasses[baseColor];
+
+    return (
+        <main className='m-6'>
+            <Link className='absolute' href='../'>Go back...</Link>
+            <div className='box text-center'>
+                <div className='huge p-2.5 rounded'>
+                    {count}
+                </div>
+                <div className='buttons'>
+                    <button
+                        className={getButtonClass('gray', isMin)}
+                        onClick={decrement}
+                    >
+                        -
+                    </button>
+                    <button
+                        className={getButtonClass('blue', isMax)}
+                        onClick={increment}
+                    >
+                        +
+                    </button>
+                </div>
             </div>
-            <div className='buttons'>
-                <button
-                    className={ 'border-4 bg-gray-600 border-gray-700 ' +
-                        'hover:bg-gray-500 hover:border-gray-600' }
-                    onClick={decrement}
-                >
-                    -
-                </button>
-                <button
-                    className={ 'border-4 bg-blue-600 border-blue-800 ' +
-                        'hover:bg-blue-500 hover:border-blue-700' }
-                    onClick={increment}
-                >
-                    +
-                </button>
-            </div>
-        </div>
-    </main>
+        </main>
+    )
 }
