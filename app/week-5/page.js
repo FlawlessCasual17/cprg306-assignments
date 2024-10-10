b8588529a99074369c03c9337086ded21ee75344
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-10-09 20:52:14 -0600

    Small update
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- app/week-5/page.js
+++ app/week-5/page.js
@@ -6,6 +6,6 @@ import NewItem from './new-item'
 export default function Page() {
     return <main>
         <Link className='absolute' href='../'>Go back...</Link>
-        <NewItem/>
+        <NewItem />
     </main>
 }
