621965b38d85fe050bf5daf28a75474bad22ee35
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-10-11 12:19:39 -0600

    Update code for `week-4` again
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -24,9 +24,9 @@ export default function Page() {
         red: 'border-4 bg-red-800 border-red-900 hover:bg-red-700 hover:border-red-800',
         blue: 'border-4 bg-blue-600 border-blue-800 hover:bg-blue-500 hover:border-blue-700',
         gray: 'border-4 bg-gray-600 border-gray-700 hover:bg-gray-500 hover:border-gray-600'
-    };
+    }
 
-    const getButtonClass = (baseColor, isLimit) => isLimit ? buttonClasses.red : buttonClasses[baseColor];
+    const getButtonClass = (baseColor, isLimit) => isLimit ? buttonClasses.red : buttonClasses[baseColor]
 
     return <main className='m-6'>
         <Link className='absolute' href='../'>Go back...</Link>
