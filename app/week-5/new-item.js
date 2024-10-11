f664099f37f211223750dc2c34be9553905be4cb
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-10-11 13:41:07 -0600

    Updated `week-5` module, not ready yet
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -1,50 +1,45 @@
 import './styles.css'
 import { useState, useEffect } from 'react'
 
-export default function NewItem() {
-    const [count, setCount] = useState(1)
+export default function NewItem({quantity, setQuantity}) {
     const [isMin, setIsMin] = useState(true)
     const [isMax, setIsMax] = useState(false)
 
     const MIN_COUNT = 1, MAX_COUNT = 20
 
     useEffect(() => {
-        setIsMin(count === MIN_COUNT)
-        setIsMax(count === MAX_COUNT)
+        setIsMin(quantity === MIN_COUNT)
+        setIsMax(quantity === MAX_COUNT)
         },
-        [count]
+        [quantity]
     )
 
-    const increment = () => setCount(count < MAX_COUNT ? count + 1 : MAX_COUNT)
-    const decrement = () => setCount(count > MIN_COUNT ? count - 1 : MIN_COUNT)
+    const increment = () => setQuantity(quantity < MAX_COUNT ? quantity + 1 : MAX_COUNT)
+    const decrement = () => setQuantity(quantity > MIN_COUNT ? quantity - 1 : MIN_COUNT)
 
     const buttonClasses = {
         red: 'border-4 bg-red-800 border-red-900 hover:bg-red-700 hover:border-red-800',
         blue: 'border-4 bg-blue-600 border-blue-800 hover:bg-blue-500 hover:border-blue-700',
         gray: 'border-4 bg-gray-600 border-gray-700 hover:bg-gray-500 hover:border-gray-600'
-    };
+    }
 
-    const getButtonClass = (baseColor, isLimit) => isLimit ? buttonClasses.red : buttonClasses[baseColor];
+    const getButtonClass = (baseColor, isLimit) => isLimit ? buttonClasses.red : buttonClasses[baseColor]
 
-    return <div className='m-6'>
+    const getButton = (color, isMax, decrement, symbol) =>
+        <button
+            className={getButtonClass(color, isMax)}
+            onClick={decrement}
+        >
+            {symbol}
+        </button>
+
+    return <span>
         <div className='box text-center'>
-            <div className='huge p-2.5 rounded'>
-                {count}
-            </div>
+            <div className='huge p-2.5 rounded'>{ quantity }</div>
             <div className='buttons'>
-                <button
-                    className={getButtonClass('gray', isMin)}
-                    onClick={decrement}
-                >
-                    -
-                </button>
-                <button
-                    className={getButtonClass('blue', isMax)}
-                    onClick={increment}
-                >
-                    +
-                </button>
+                {getButton('gray', isMin, decrement, '-')}
+                {getButton('blue', isMax, increment, '+')}
             </div>
         </div>
-    </div>
+    </span>
 }
--- app/week-5/page.js
+++ app/week-5/page.js
@@ -1,11 +1,34 @@
 "use client"
 import './styles.css'
-import Link from 'next/link'
 import NewItem from './new-item'
+import Link from 'next/link'
+import { useState } from 'react'
+
+function handleSubmit(name, quantity, category) {
+    const item = {
+        name: name,
+        quantity: quantity,
+        category: category
+    }
+
+    console.log(item)
+
+    alert(`Added item: ${name}, quantity: ${quantity}, category: ${category}`)
+}
 
 export default function Page() {
-    return <main>
+    const [name, setName] = useState('')
+    const [quantity, setQuantity] = useState(1)
+    const [category, setCategory] = useState('produce')
+
+    return <main className='m-6'>
         <Link className='absolute' href='../'>Go back...</Link>
-        <NewItem />
+        <form className='p-2 m-4 bg-slate-900 max-w-sm w-full'>
+            <div className='mb-2'>
+                <input className='class="w-full mt-1 border-2 border-gray-300 p-2 rounded-lg'
+                    placeholder='Name of Item' required />
+            </div>
+            <NewItem quantity={quantity} setQuantity={setQuantity} />
+        </form>
     </main>
 }
