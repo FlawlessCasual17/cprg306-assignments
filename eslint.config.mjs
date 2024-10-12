bb4c5919413ce015f378e61ea5f58beb7915676d
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-10-12 15:17:43 -0600

    Update stuff
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+import stylisticJs from '@stylistic/eslint-plugin-js'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory : __dirname,
+  recommendedConfig : js.configs.recommended,
+  allConfig : js.configs.all
+});
+
+export default [...compat.extends('next/core-web-vitals'), {
+  plugins : {
+    '@stylistic/js' : stylisticJs
+  },
+  rules : {
+    quotes : ['warn', 'single'],
+    'jsx-quotes' : ['warn', 'prefer-single'],
+    'quote-props' : ['error', 'as-needed'],
+    '@stylistic/js/object-curly-spacing' : ['error', 'always', { objectsInObjects : false }]
+  }
+}];
