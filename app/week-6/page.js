11b45afb38e86baf6a49010b24c9e85d17843094
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-10-13 19:42:58 -0600

    Create new module `week-6`
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

---
+++ app/week-6/page.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Page() {
+    return (
+        <main className='m-6'>
+            {/* dprint-ignore */}
+            <Link href='../'>Go back...</Link>
+        </main>
+    )
+}
