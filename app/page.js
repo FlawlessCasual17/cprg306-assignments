9397772ab9a5bc750bf1199e4fca30b5853395f3
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-10-17 21:14:23 -0600

    Update `app/page.js`
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- app/page.js
+++ app/page.js
@@ -15,11 +15,11 @@ export default function Page() {
 
             {/* dprint-ignore */}
             <ul className='m-6' style={{ marginRight: '55rem' }}>
-            <li><Link href='week-2'>Week 2</Link></li>
-            <li><Link href='week-3'>Week 3</Link></li>
-            <li><Link href='week-4'>Week 4</Link></li>
-            <li><Link href='week-5'>Week 5</Link></li>
-        </ul>
+                <li><Link href='week-2'>Week 2</Link></li>
+                <li><Link href='week-3'>Week 3</Link></li>
+                <li><Link href='week-4'>Week 4</Link></li>
+                <li><Link href='week-5'>Week 5</Link></li>
+            </ul>
         </main>
     )
 }
