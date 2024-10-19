dc517206fd88cfba7d6a60c6cae845dd76fddc47
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-10-18 21:30:06 -0600

    Revert "Update `week-6` module"
    This reverts commit b71411aa3dee3838b23693d95f7e3b1574c82dd0.

--- app/week-6/list-item.js
+++ app/week-6/list-item.js
@@ -11,7 +11,7 @@ export default function ListItem() {
 
     function sortItems(type) {
         setSortBy(type)
-        if (sortBy === 'name')
+        if (type === 'name')
             setItems([...items].sort((a, b) => a.name.localeCompare(b.name)))
         else
             setItems([...items].sort((a, b) => a.category.localeCompare(b.category)))
