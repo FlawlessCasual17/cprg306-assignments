73e3282f21e7393a05da8fc37e17ea673c891a68
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-10-18 20:53:38 -0600

    Update `week-6` module
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- app/week-6/item.js
+++ app/week-6/item.js
@@ -2,7 +2,7 @@ import './styles.css'
 
 export default function Item({ id, name, quantity, category }) {
     return (
-        <li className='relative w-64 mb-5 ring-2 ring-gray-300 p-4 rounding'>
+        <li key={id} className='relative w-64 mb-5 ring-2 ring-gray-300 p-4 rounding'>
             <div className='font-semibold text-lg'>
                 {name} <span className='font-normal text-gray-600' style={{ lineHeight: '0', fontSize: '1rem' }}>
                     × {quantity}
