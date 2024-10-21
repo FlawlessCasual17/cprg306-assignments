bd385ca4b0fc23cd5111ee74fffb110a0f461b21
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-10-20 23:34:21 -0600

    Updates to styling for `week-6`
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- app/week-6/item.js
+++ app/week-6/item.js
@@ -20,7 +20,7 @@ export default function Item({ id, name, quantity, category }) {
                 {category}
             </div>
             <li className='relative w-64 mb-5 ring-2 ring-gray-300 p-4 rounded-xl'>
-                <div className='font-semibold text-lg'>
+                <div className='font-bold text-lg'>
                     {name} <span className='quantity'>× {quantity}</span>
                 </div>
                 {/* <div className='text-gray-600' style={{ fontSize: '1' }}>{category}</div> */}
