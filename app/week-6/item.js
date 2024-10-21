e6b40b8b916e2865198c08553baf6ef98d4c98a1
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-10-20 21:41:44 -0600

    Updates
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- app/week-6/item.js
+++ app/week-6/item.js
@@ -2,10 +2,11 @@ export default function Item({ id, name, quantity, category }) {
     return (
         <li className='relative w-64 mb-5 ring-2 ring-gray-300 p-4 rounded-xl'>
             <div className='font-semibold text-lg'>
-                {!name ? null : name} {quantity && // Don't render anything if there's no name
+                {/* Don't render anything if there's no name, or no quantity */}
+                {!name ? null : name} {quantity &&
                     <span className='quantity'>× {quantity}</span>}
             </div>
-            {category &&
+            {category && // Don't render anything if there's no category
                 <div className='text-gray-600' style={{ fontSize: '1rem' }}>{category}</div>
             }
         </li>
