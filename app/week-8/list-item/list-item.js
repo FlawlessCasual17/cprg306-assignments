1e9846edff4fa2b940345f5326e743125c4de0dc
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-10-30 21:52:57 -0600

    Add `week-8` page
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

---
+++ app/week-8/list-item/list-item.js
@@ -0,0 +1,48 @@
+'use client'
+import { useEffect, useState } from 'react'
+import '../styles.css'
+import Item from './item'
+import jsonItems from './items.json'
+
+export default function ListItem({ initialItems }) {
+    const [sortBy, setSortBy] = useState('name')
+    const [items, setItems] = useState([...jsonItems, ...initialItems])
+
+    useEffect(() => {
+        setItems([...jsonItems, ...initialItems])
+    }, [initialItems])
+
+    function sortByName() {
+        setSortBy('name')
+        setItems([...items].sort((a, b) => a.name.localeCompare(b.name)))
+    }
+
+    function sortByCategory() {
+        setSortBy('category')
+        setItems([...items].sort((a, b) => a.category.localeCompare(b.category)))
+    }
+
+    console.log(`sortBy is now equal to: ${sortBy}`)
+
+    // const getClasses = (type) =>
+    //     `flex-buttons rounded-xl ${sortBy === type && 'flex-buttons-active'}`
+
+    return (
+        <span>
+            <div className='flex justify-center button-container relative'>
+                <button onClick={sortByName} className={`flex-buttons rounded-xl ${sortBy === 'name' && 'flex-buttons-active'}`}>
+                    Sort by Name
+                </button>
+                <button onClick={sortByCategory} className={`flex-buttons rounded-xl ${sortBy === 'category' && 'flex-buttons-active'}`}>
+                    Sort by Category
+                </button>
+            </div>
+            <div className='relative'>
+                {/* {[...items].map((item, id) => (<div key={id}>{item.category}</div>))} */}
+                <ul className='flexbox-list rounded-xl align-text-bottom'>
+                    {[...items].map(item => (<Item key={item.id} {...item} />))}
+                </ul>
+            </div>
+        </span>
+    )
+}
