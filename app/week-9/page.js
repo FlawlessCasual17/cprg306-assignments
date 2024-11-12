3bb085cca166ec46143d272b259b8baf63466db2
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-11-11 21:11:38 -0700

    Finished week-9
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

---
+++ app/week-9/page.js
@@ -0,0 +1,58 @@
+'use client'
+import Link from 'next/link'
+import { useUserAuth } from './_utils/auth-context'
+import './styles.css'
+
+export default function Page() {
+    const { user, gitHubSignIn, firebaseSignOut } = useUserAuth()
+
+    async function handleSignIn() {
+        try {
+            await gitHubSignIn()
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    async function handleSignOut() {
+        try {
+            await firebaseSignOut()
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const isSignedIn = !user
+
+    // This has to be a function or else it won't render
+    const guestPage = () => (
+        <button onClick={handleSignIn} className='signIn rounded-xl transition-colors'>
+            Sign In with GitHub
+        </button>
+    )
+
+    const userPage = () => (
+        <div className='text-center'>
+            <h2 className='mb-4'>
+                Welcome, {user.displayName}
+            </h2>
+            <Link href='/week-9/shopping-list' className='block mb-4 text-blue-500 hover:text-blue-700'>
+                Go to Shopping List
+            </Link>
+            <button onClick={handleSignOut} className='signOut rounded-xl transition-colors'>
+                Sign Out
+            </button>
+        </div>
+    )
+
+    return (
+        <main className='m-6'>
+            <Link href='../'>
+                Go back...
+            </Link>
+            <div className='flex flex-col items-center mt-4'>
+                {isSignedIn ? guestPage() : userPage()}
+            </div>
+        </main>
+    )
+}
--- app/week-9/shopping-list/page.js
+++ app/week-9/shopping-list/page.js
@@ -1,12 +1,29 @@
 'use client'
-import { useState } from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
 import ButtonsWithCategories from '../../week-7/buttons-category/buttons-category'
 import ListItem from '../../week-8/list-item/list-item'
 import '../../week-8/styles.css'
+import { useUserAuth } from '../_utils/auth-context'
 
 export default function Page() {
+    const { user } = useUserAuth()
+    const router = useRouter()
     const [items, setItems] = useState([])
+
+    useEffect(() => {
+        const isSignedIn = !user
+
+        if (isSignedIn) {
+            alert('Please sign in to access the shopping list!')
+            router.push('../../week-9')
+        }
+    }, [user, router])
+
+    if (!user) {
+        return null
+    }
 
     const handleAddItem = (newItem) => {
         setItems([...items, { ...newItem, id: items.length + 1 }])
